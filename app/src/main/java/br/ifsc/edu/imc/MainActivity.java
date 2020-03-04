48b5e0a457ef679aac61f5646ffdec0a06e1d87d
Computador ifsc <ifsc@ifsc.com.br>
2020-03-03 21:37:12 -0300

    add images and a seekBar to represent state of people

--- app/src/main/java/br/ifsc/edu/imc/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/imc/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextHeight;
     Button btnCalc;
     TextView txtViewIMC;
+    ImageView image;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +28,37 @@ public class MainActivity extends AppCompatActivity {
         editTextHeight = findViewById(R.id.editTextHeight);
         btnCalc = findViewById(R.id.btnCalc);
         txtViewIMC = findViewById(R.id.txtViewIMC);
+        image  = findViewById(R.id.image);
+        seekBar = findViewById(R.id.seekBar);
 
         btnCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txtViewIMC.setText(String.format("%.2f", calcImc(Double.parseDouble(editTextWeight.getText().toString()),Double.parseDouble(editTextHeight.getText().toString()))));
+                double value = calcImc(Double.parseDouble(editTextWeight.getText().toString()),Double.parseDouble(editTextHeight.getText().toString()));
+
+                txtViewIMC.setText(String.format("%.2f", value));
+
+                seekBar.setProgress((int) value);
+
+                if (value <= 18.5){
+                    image.setImageResource(R.drawable.abaixopeso);
+                }else if (value > 18.5 && value <= 24.9){
+                    image.setImageResource(R.drawable.normal);
+                } else if(value >= 25  && value <= 29.9){
+                    image.setImageResource(R.drawable.normal);
+                } else if (value > 30 && value <= 34.9){
+                    image.setImageResource(R.drawable.obesidade1);
+                } else if (value >= 35 && value <= 39.9){
+                    image.setImageResource(R.drawable.obesidade2);
+                } else if (value >= 40){
+                    image.setImageResource(R.drawable.obesidade3);
+                }
+
             }
         });
     }
 
-    public Double calcImc(Double weight, Double height){
+    public double calcImc(Double weight, Double height){
         if ( height != 0 ) {
             return weight / (height * height);
         }
